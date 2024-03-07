a82d819d9d020ff8f784a9e4f3ea16dc4c6361e0
sarahsteadman <fairyfaye4real@gmail.com>
2024-03-07 14:34:42 -0700

    Week 2 home and product pages finished

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 function convertToJson(res) {
@@ -59,45 +58,3 @@ export default class ProductDetails {
   }
 
 }
-=======
-import {setLocalStorage, renderTemplate} from "./utils.mjs";
-
-function productDetailsTemplate(product){
-  const newProduct = `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-        <h2 class="divider">${product.NameWithoutBrand}</h2>
-        <img
-          class="divider"
-          src="${product.Image}"
-          alt="${product.NameWithoutBrand}"
-        />
-        <p class="product-card__price">$${product.FinalPrice}</p>
-        <p class="product__color">${product.Colors[0].ColorName}</p>
-        <p class="product__description">
-        ${product.DescriptionHtmlSimple}
-        </p>
-      <div class="product-detail__add">
-        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-      </div></section>`;
-    return newProduct;
-}
-  
-  export default class ProductDetails {
-    constructor(productId, dataSource){
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
-      }
-    async init() {
-        this.product = await this.dataSource.findProductById(this.productId)
-        this.renderProductDetails("main");
-        document
-          .getElementById('addToCart')
-          .addEventListener('click', this.addToCart.bind(this));
-        }
-    addToCart() {
-        setLocalStorage("so-cart", this.product);
-    }
-    renderProductDetails(selector){
-      renderTemplate(productDetailsTemplate, selector, this.product);}
-    }
->>>>>>> 2fb9721e9107375a34e5a40811e15869e43a63d8
