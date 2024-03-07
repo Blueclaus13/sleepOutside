c3cde054e58a87b0776400c1a51e78ffadaaf8b8
sarahsteadman <fairyfaye4real@gmail.com>
2024-03-07 16:34:33 -0700

    deleted extra add to cart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -8,13 +8,3 @@ const dataSource = new ProductData("tents");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-console.log(dataSource.findProductById(productId));
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCartHandler);
