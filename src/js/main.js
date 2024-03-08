5a611a6a88932372ea643e7fd68deab8c092770b
PatrizioTheDev <patriziomedley@gmail.com>
2024-03-07 23:04:15 -0400

    gg

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const data = ProductData
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductList("Tents", dataSource, element);
+
+listing.init();
