fd8fe86f93d03adf3cf0abc064b9134396084b47
claudia.madrid <claudia.madrid@choicehotels.com>
2024-03-09 07:47:03 -0600

    change Cart fuction to a class

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,38 +1,9 @@
+import RenderCart from "./productCart";
 import { getLocalStorage } from "./utils.mjs";
 
-function renderCartContents() {
-  //initialize the variable with an empty array if the LocalStorage is empty.
-  const cartItems = getLocalStorage("so-cart") ?? [];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+const cart = new RenderCart(getLocalStorage("so-cart"));
+cart.init();
 
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <div class="cart-closed"><button id="${item.Id}">X</button></div>
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
-function onClosed( itemId){
-  console.log(itemId.target.id + "Item to delete");
-
-}
-
-renderCartContents();
-const button = document.getElementsByTagName("button");
-button.addEventListener("click", onClosed);
+// function onClosed( itemId){
+//   console.log(itemId.target.id + "Item to delete");
+// }
---
+++ src/js/productCart.js
@@ -0,0 +1,50 @@
+import {setLocalStorage, renderListWithTemplate} from "./utils.mjs";
+
+function cartItemTemplate(item) {
+    const newItem = `<li class="cart-card divider">
+    <div class="cart-closed"><button id="${item.Id}">X</button></div>
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
+  
+    return newItem;
+  }
+  
+  export default class RenderCart {
+    constructor(cartItems){
+      this.cartItems = cartItems ?? [];
+    }
+  
+    async init() {
+      const element = document.querySelector(".product-list");
+      this.renderProducts(element);
+      console.log(this.getProductsId())
+    }
+    //initialize the variable with an empty array if the LocalStorage is empty.
+    // const cartItems = getLocalStorage("so-cart") ?? [];
+    // const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    // document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  
+    renderProducts(element){
+      renderListWithTemplate(cartItemTemplate, element, this.cartItems);
+    }
+    getProductsId(){
+        const idList = this.cartItems.map((item) => item.Id);
+        return idList;
+    }
+  
+    deleteItem(){
+  
+    }
+  
+  }
