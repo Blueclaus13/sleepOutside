b8d1fd80adf64e2a293c1f76fca28118c7296fc8
PatrizioTheDev <patriziomedley@gmail.com>
2024-03-09 13:14:51 -0400

    Cart remove

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,11 +2,21 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const htmlItems = cartItems.map((item, index) => cartItemTemplate(item, index));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+ 
+  document.querySelectorAll(".remove-item").forEach(button => {
+    button.addEventListener("click", (event) => {
+      const itemIndex = parseInt(event.target.dataset.itemIndex, 10);
+      cartItems.splice(itemIndex, 1);
+      localStorage.setItem("so-cart", JSON.stringify(cartItems));
+      renderCartContents();
+    });
+  });
 }
 
-function cartItemTemplate(item) {
+function cartItemTemplate(item, index) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -20,6 +30,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="remove-item" data-item-index="${index}">X</button>
 </li>`;
 
   return newItem;
