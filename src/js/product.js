c97a43ad216e4a81f610bc4b5f6c52e1d62718db
Claudia Madrid <71229725+Blueclaus13@users.noreply.github.com>
2024-03-09 09:19:52 -0600

    Revert "Merge pull request #3 from Blueclaus13/SS-Week2"
    This reverts commit fee86b1fc632700f5d72ec754fae628a789f7553, reversing
    changes made to c0a0873f46b9e3fef00e01c18871cd380b847be1.

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,24 @@
+<<<<<<< HEAD
 import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
+=======
+import { getParams} from "./utils.mjs";
+>>>>>>> 2fb9721e9107375a34e5a40811e15869e43a63d8
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParams("product");
+<<<<<<< HEAD
+const productId = getParams('product');
+=======
+>>>>>>> 2fb9721e9107375a34e5a40811e15869e43a63d8
 const dataSource = new ProductData("tents");
+const productId = getParams("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+<<<<<<< HEAD
 
 console.log(dataSource.findProductById(productId));
+
 
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -17,4 +27,8 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+document
+  .getElementById("addToCart")
+// .addEventListener("click", addToCartHandler);
+=======
+>>>>>>> 2fb9721e9107375a34e5a40811e15869e43a63d8
