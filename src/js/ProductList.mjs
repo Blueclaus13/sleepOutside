ff8a8fdc199671c373a8cd81fbba16989082e15d
PatrizioTheDev <patriziomedley@gmail.com>
2024-03-16 19:13:31 -0400

    gggg

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,31 +1,42 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
-function productCardTemplate(product){
-    const newProduct = `<li class="product-card">
-        <a href="product_pages/index.html?product=${product.Id}">
-            <img src="${product.Image}" alt="Image of ${product.Name}">
-            <h3 class="card_brand">${product.Brand.Name}</h3>
-            <h2 class="card_name">${product.Name}</h2>
-            <p class="product-card_price">$${product.FinalPrice}</p>
-        </a>
-    </li>`;
-
-    return newProduct;
+function productCardTemplate(product) {
+  return `<li class="product-card">
+  <a href="/product_pages/index.html?product=${product.Id}">
+  <img
+    src="${product.Images.PrimaryMedium}"
+    alt="Image of ${product.Name}"
+  />
+  <h3 class="card__brand">${product.Brand.Name}</h3>
+  <h2 class="card__name">${product.Name}</h2>
+  <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
 }
 
 export default class ProductList {
-    constructor(category, datasource, listElement){
-        this.category = category;
-        this.datasource = datasource;
-        this.listElement = listElement;
-    }
+  constructor(category, dataSource, listElement) {
+    // We passed in this information to make our class as reusable as possible.
+    // Being able to define these things when we use the class will make it very flexible
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+  async init() {
+    // our dataSource will return a Promise...so we can use await to resolve it.
+    const list = await this.dataSource.getData(this.category);
+    // render the list
+    this.renderList(list);
+    //set the title to the current category
+    document.querySelector(".title").innerHTML = this.category;
+  }
+  // render after doing the first stretch
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 
-    async init() {
-        const list = await this.datasource.getData();
-        this.renderList(list);
-    }
-
-    renderList(list){
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
+  // render before doing the stretch
+  // renderList(list) {
+  //   const htmlStrings = list.map(productCardTemplate);
+  //   this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+  // }
 }
