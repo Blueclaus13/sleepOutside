e633a07cba9c957e80d64be5d3ca62dd1a5c8c02
sarahsteadman <fairyfaye4real@gmail.com>
2024-03-23 13:24:53 -0600

    Changed single quotes to double quotes

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -5,7 +5,7 @@ import { getParams, loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
 const category = getParams("category");
 const dataSource = new ExternalServices();
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 const myList = new ProductList(category, dataSource, listElement);
 
 myList.init();
--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
 import { getLocalStorage, getParams, loadHeaderFooter } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
-import ProductDetails from './ProductDetails.mjs';
+import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
 const dataSource = new ExternalServices("tents");
-const productId = getParams('product');
+const productId = getParams("product");
 
 const product = new ProductDetails(productId, dataSource, getLocalStorage("so-cart"));
 product.init();
